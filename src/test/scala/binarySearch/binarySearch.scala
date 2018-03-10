950bc75f136f8d4f67ad6e28ee2049a9c29cee67
bigpas <pasqui.dente@gmail.com>
2018-03-10 20:49:55 +0000

    test sorting

--- src/test/scala/binarySearch/binarySearch.scala
+++ src/test/scala/binarySearch/binarySearch.scala
@@ -13,6 +13,11 @@ class binarySearchTest extends FunSuite {
     val resTest02 = binarySearch.binarySearch(data = Vector(1, 2, 4, 5, 7, 10, 33, 200, 1000), target = 117)
     assert(resTest02 == None)
   }
+
+  test("binarySearch - sort required"){
+    val resTest01 = binarySearch.binarySearch(data = Vector(10, 33, 200, 1000, 1, 2, 4, 5, 7), target = 5)
+    assert(resTest01 == Some(3))
+  }
   
 }
 
