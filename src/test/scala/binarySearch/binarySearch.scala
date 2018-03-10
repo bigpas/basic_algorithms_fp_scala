3a66c2423fe7704d4f3833852751f677879bbe57
bigpas <pasqui.dente@gmail.com>
2018-03-10 19:03:37 +0000

    minor: clean up

--- src/test/scala/binarySearch/binarySearch.scala
+++ src/test/scala/binarySearch/binarySearch.scala
@@ -4,17 +4,6 @@ import org.scalatest.FunSuite
 
 class binarySearchTest extends FunSuite {
 
-  // describe("A Pizza") {
-
-  //   it("should start with no toppings") {
-  //     assert(pizza.getToppings.size == 0)
-  //   }
-
-  //   it("should allow addition of toppings") (pending)
-
-  //   it("should allow removal of toppings") (pending)
-  // }
-
   test("binarySearch - successful case"){
     val resTest01 = binarySearch.binarySearch(data = Vector(1, 2, 4, 5, 7, 10, 33, 200, 1000), target = 5)
     assert(resTest01 == Some(3))
